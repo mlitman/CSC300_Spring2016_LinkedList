614b5d50f0f6aed0e20681cc4ff5009dd9eb564d
Mike Litman <mike@awesomefat.com>
2016-02-04 11:22:58 -0600

    new branch for HW #5

--- app/src/main/java/com/example/awesomefat/linkedlist/LinkedList.java
+++ app/src/main/java/com/example/awesomefat/linkedlist/LinkedList.java
@@ -11,17 +11,17 @@ public class LinkedList
 {
     private Node head;
     private LinearLayout layout;
-
+    public int count;
     public LinkedList(LinearLayout layout)
     {
         this.head = null;
         this.layout = layout;
+        this.count = 0;
     }
 
     public void display()
     {
-        //changed
-        //this.layout.removeAllViews();
+        this.layout.removeAllViews();
         if(this.head == null)
         {
             View v = ListCore.inflater.inflate(R.layout.node, null);
@@ -40,12 +40,36 @@ public class LinkedList
         this.layout.addView(v);
     }
 
+    public Node getAtIndex(int i)
+    {
+        return null;
+    }
+
+    //inefficient, but accurate
+    public int count()
+    {
+        int count = 0;
+        if(head != null)
+        {
+            count++;
+            //at least one node in the list
+            Node currNode = head;
+            while(currNode.getNextNode() != null)
+            {
+                currNode = currNode.getNextNode();
+                count++;
+            }
+        }
+        return count;
+    }
+
     public void addFront(String value)
     {
         //this adds a new Node to the front of the list with payload == value
         Node n = new Node(value);
         n.setNextNode(this.head);
         this.head = n;
+        this.count++;
     }
 
     public Node removeFront()
@@ -57,8 +81,63 @@ public class LinkedList
         if(this.head != null)
         {
             this.head = this.head.getNextNode();
+            nodeToReturn.setNextNode(null);
+            this.count--;
         }
-        nodeToReturn.setNextNode(null);
+
         return nodeToReturn;
     }
+
+    public void addEnd(String value)
+    {
+        if(this.head == null)
+        {
+            this.addFront(value);
+        }
+        else
+        {
+            Node n = new Node(value);
+            Node currNode = head;
+            while(currNode.getNextNode() != null)
+            {
+                currNode = currNode.getNextNode();
+            }
+            //currNode is currently pointing at the last node in the list
+            currNode.setNextNode(n);
+            this.count++;
+        }
+    }
+
+    public Node removeEnd()
+    {
+        if(head == null)
+        {
+            return head;
+        }
+        else
+        {
+            this.count--;
+            Node nodeToReturn = head;
+            //deal with the 1-list special case
+            if(head.getNextNode() == null)
+            {
+                head = null;
+                return nodeToReturn;
+            }
+            else
+            {
+                //there are at least 2 nodes in the list
+                Node currNode = head;
+                while (currNode.getNextNode() != null && currNode.getNextNode().getNextNode() != null)
+                {
+                    currNode = currNode.getNextNode();
+                }
+                //currNode points to the Node right before the last node in the list
+                nodeToReturn = currNode.getNextNode();
+                currNode.setNextNode(null);
+                return nodeToReturn;
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/awesomefat/linkedlist/MainActivity.java
+++ app/src/main/java/com/example/awesomefat/linkedlist/MainActivity.java
@@ -4,12 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
+    private Button addFrontButton;
+    private Button addEndButton;
+    private Button removeFrontButton;
+    private Button removeEndButton;
+    private EditText newValueEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -17,18 +23,33 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_main);
 
+        //give ourselves programmatic access to the buttons and edit text
+        this.addFrontButton = (Button)this.findViewById(R.id.addFrontButton);
+        this.addEndButton = (Button)this.findViewById(R.id.addEndButton);
+        this.removeFrontButton = (Button)this.findViewById(R.id.removeFrontButton);
+        this.removeEndButton = (Button)this.findViewById(R.id.removeEndButton);
+        this.newValueEditText = (EditText)this.findViewById(R.id.newValueET);
+
         ListCore.inflater = this.getLayoutInflater();
 
         LinearLayout svLL = (LinearLayout)this.findViewById(R.id.scrollViewLL);
         LinkedList ll = new LinkedList(svLL);
-        ll.addFront("1");
-        ll.addFront("2");
-        ll.addFront("3");
-        ll.addFront("4");
-        ll.display();
+        //ll.addFront("1");
+        //ll.addFront("2");
+        //ll.addFront("3");
+        //ll.addFront("4");
+        //ll.display();
         ll.removeFront();
         ll.display();
+        ll.addEnd("6");
+        ll.addEnd("7");
+        ll.addEnd("8");
+        ll.display();
+        ll.removeEnd();
+        ll.display();
 
+        System.out.println("**** " + ll.count);
+        System.out.println("**** " + ll.count());
         /*
         View v;
         for(int i = 0; i < 10; i++)
@@ -41,8 +62,25 @@ public class MainActivity extends AppCompatActivity
         */
     }
 
-    public void theButtonClicked(View sender)
+    public void aButtonClicked(View sender)
     {
-        //I want to change the text on my label
+        if(sender == this.addFrontButton)
+        {
+            System.out.println("**** add front button pressed");
+            System.out.println("adding: " + this.newValueEditText.getText());
+        }
+        else if(sender == this.addEndButton)
+        {
+            System.out.println("**** add end button pressed");
+            System.out.println("adding: " + this.newValueEditText.getText());
+        }
+        else if(sender == this.removeFrontButton)
+        {
+            System.out.println("**** remove front button pressed");
+        }
+        else if(sender == this.removeEndButton)
+        {
+            System.out.println("**** remove end button pressed");
+        }
     }
 }
