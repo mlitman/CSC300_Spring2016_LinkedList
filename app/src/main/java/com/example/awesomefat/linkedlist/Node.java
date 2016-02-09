5230fab8556cc17b229e86b65c5fdf30df166d60
Mike Litman <mike@awesomefat.com>
2016-02-09 11:33:47 -0600

    new branch for HW #6

--- app/src/main/java/com/example/awesomefat/linkedlist/Node.java
+++ app/src/main/java/com/example/awesomefat/linkedlist/Node.java
@@ -13,7 +13,7 @@ public class Node
     private String payload;
     private Node nextNode;
     private TextView valueLabel;
-    private LinearLayout layout;
+    private LinearLayout theLayout;
 
     public Node(String payload)
     {
@@ -22,11 +22,33 @@ public class Node
 
     }
 
+    public void textClicked(View v)
+    {
+        System.out.println("***** Text Was Clicked: " + this.payload);
+    }
+
     public void display(LinearLayout layout)
     {
+        final Node myself = this;
+        this.theLayout = layout;
         View v = ListCore.inflater.inflate(R.layout.node, null);
-        TextView tf = (TextView) v.findViewById(R.id.theValueTF);
-        tf.setText(this.payload);
+        this.valueLabel = (TextView) v.findViewById(R.id.theValueTF);
+        this.valueLabel.setText(this.payload);
+        this.valueLabel.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //Right now, we "hide" this text label and prove that we can find "myself"
+                //in the Linked List.  Update this to remove myself from the linked list
+                //and then ask the list to display itself, which should update the entire
+                //view.  That is to say, we will no longer need to hide ourself.
+                System.out.println("***** Text Was Clicked: " + payload);
+                valueLabel.setVisibility(View.GONE);
+                System.out.println("**** Position = " + ListCore.ll.indexOf(myself));
+
+            }
+        });
         layout.addView(v);
 
         if(this.nextNode != null)
